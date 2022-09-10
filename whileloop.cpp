782fdc116807cd0cca6d5356818ab28867771058
koemnak <heatkimnak@gmail.com>
2022-09-10 23:14:38 +0700

    commit

---
+++ whileloop.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i=0;
+    while(i<=10){
+        
+        cout<<i<<endl;
+        i++;
+        
+        if(i==6){
+            
+            cout<<i<<endl;
+            i-=3;
+
+        }
+        
+    }
+    return 0;
+}
